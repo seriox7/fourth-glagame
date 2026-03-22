2f0339d7f0690ecb4639478bda62d14c69e8928e
sseerriioooxx <2540198610@qq.com>
2026-03-22 17:56:24 +0800

    文本，角色动画转换

---
+++ scripts/character.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+const CHARACTER_FRAMES = {
+	"林峰":preload("res://resourse/林峰_sprite_frames.tres"),
+	"杨坤":preload("res://resourse/杨坤_sprite_frames.tres"),
+	"勃林":preload("res://resourse/勃林_sprite_frames.tres"),
+	"你":preload("res://resourse/你_sprite_frames.tres"),
+	"旁白":preload("res://resourse/旁白_sprite_frames.tres")
+}
+
+func _ready() -> void:
+	pass
+
+func change_character(character_name:String,is_talking : bool = true):
+	sprite.sprite_frames = CHARACTER_FRAMES[character_name]
+	if is_talking:
+		sprite.play("speak")
+	else:
+		sprite.play("idle")
--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -7,7 +7,23 @@ var dialog_index :int = 0
 const dialog_lines : Array[String] = [
 	"林峰:早上好哟，xx，该起床了",
 	"杨坤:这现在几点啊",
-	"柏林:zzz",
+	"勃林:zzz",
+	"你:草，还要上早八服了，好困啊(下床)",
+	"杨坤:(刷牙)",
+	"勃林:(上半身裸露，眯着眼睛)",
+	"旁白:你遵从这平日的习惯，刷牙洗脸，拉泡屎💩，出门，坐着 林峰 gege的电动车去上早八了",
+	"旁白:你坐着他的车，能亲切的感受到他的体温，想起这个，你不时脸红了起来",
+	"xx:我草(急刹车)",
+	"旁白:由于惯性，你不得不抱住xx的腰",
+	"你:(好温暖)",
+	"xx:你没事吧",
+	"你:没事(你脸红的急忙地松开了手)",
+	"你:心想，哇，xx君好温柔哇",
+	"旁白:到了教室，旁边正坐着的是杨坤，林峰，勃林，因为早上是水课，
+	所以大家都在玩手机，你瞅了瞅杨坤，只见他打开了海岛奇兵，你看着他的侧颜，不由地轻哼了起来",
+	"你:哇，杨坤哥好帅啊",
+	"旁白:只见他手指轻轻一动，就把敌方给击破了，你又看了看勃林，拿着手机看着抖音，时不时憋着笑",
+	"你:勃林哥是在做憋笑挑战吗",
 ]
 
 func _ready() -> void:
@@ -33,3 +49,4 @@ func process_current_line():
 	var line_info = parse_line(line)
 	dialog_ui.speaker_name.text = line_info["speaker_name"]
 	dialog_ui.dialog_line.text = line_info["dialog_line"]
+	character.change_character(line_info["speaker_name"])
